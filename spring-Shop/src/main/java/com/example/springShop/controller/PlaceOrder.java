819eaa18683afbbfd05f01ea61cd76a751e71546
thushanm <122750947+thushanm@users.noreply.github.com>
2024-02-18 20:55:48 +0530

    Monolothicoky

--- spring-Shop/src/main/java/com/example/springShop/controller/PlaceOrder.java
+++ spring-Shop/src/main/java/com/example/springShop/controller/PlaceOrder.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/order")
+@RequestMapping("api/v1/orders")
 public class PlaceOrder {
     @Autowired
     private OrderService orderService;
